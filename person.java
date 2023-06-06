966c41f51ce984bd46d5bdb49fe0795f3d12a964
rohith5046 <135267544+rohith5046@users.noreply.github.com>
2023-06-06 08:24:02 -0400

    Create person.java

---
+++ person.java
@@ -0,0 +1,15 @@
+package practice.java;
+
+public class Person {
+
+	public static void main(String[] args) {
+		public class Person {
+			int personid;
+			int personphnnum;
+			String personname;
+		    String personaddress;
+		   
+
+	}
+
+}
