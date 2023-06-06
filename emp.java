951ea009fc5220350ec5ece6a0bd7258d8c989e3
rohith5046 <135267544+rohith5046@users.noreply.github.com>
2023-06-06 08:22:42 -0400

    Create emp.java

---
+++ emp.java
@@ -0,0 +1,15 @@
+package practice.java;
+
+public class emp {
+
+	public static void main(String[] args) {
+		int empID;
+		int empage;
+	    float empsal;
+	    boolean retired;
+	    
+		
+
+	}
+
+}
