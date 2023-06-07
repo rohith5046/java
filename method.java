703052f4dbeca5aff3b3e3328f326317ded4dcc7
rohith5046 <135267544+rohith5046@users.noreply.github.com>
2023-06-07 08:17:51 -0400

    Create method.java

---
+++ method.java
@@ -0,0 +1,11 @@
+package practice.java;
+
+public class Bank {
+
+ public static void greetcustomer() {
+System.out.println("Hello welcome to bank");
+	  }
+ public static void main(String[] args) {
+	 greetcustomer();
+}
+}
